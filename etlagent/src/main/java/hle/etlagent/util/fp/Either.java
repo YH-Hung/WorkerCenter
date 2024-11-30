bdbd01f93e5d565d9845061715607691078c86f8
yinghanhung <hle.infdy@gmail.com>
2024-11-30 23:39:19 +0800

    upgrade spring boot version and namings

--- etlagent/src/main/java/hle/etlagent/util/fp/Either.java
+++ etlagent/src/main/java/hle/etlagent/util/fp/Either.java
@@ -2,37 +2,36 @@ package hle.etlagent.util.fp;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
-public class Either<Left, Right> {
+public class Either<L, R> {
 
-    private final Left leftValue;
-    private final Right rightValue;
+    private final L leftValue;
+    private final R rightValue;
     private final EitherChoice choice;
 
 
-    private Either(Left leftValue, Right rightValue, EitherChoice choice) {
+    private Either(L leftValue, R rightValue, EitherChoice choice) {
         this.leftValue = leftValue;
         this.rightValue = rightValue;
         this.choice = choice;
     }
 
-    public static <Left, Right> Either<Left, Right> ofRight(Right right) {
-        return new Either<>(null, right, EitherChoice.RIGHT);
+    public static <L, R> Either<L, R> ofRight(R r) {
+        return new Either<>(null, r, EitherChoice.RIGHT);
     }
 
-    public static <Left, Right> Either<Left, Right> ofLeft(Left left) {
+    public static <L, R> Either<L, R> ofLeft(L left) {
         return new Either<>(left, null, EitherChoice.LEFT);
     }
 
-    public <KRight> Either<Left, KRight> map(Function<Right, KRight> transformer) {
+    public <K> Either<L, K> map(Function<? super R, ? extends K> transformer) {
         return switch (choice) {
             case LEFT -> ofLeft(leftValue);
             case RIGHT -> ofRight(transformer.apply(rightValue));
         };
     }
 
-    public Either<Left, Right> tap(Consumer<Right> consumer) {
+    public Either<L, R> tap(Consumer<? super R> consumer) {
         return switch (choice) {
             case LEFT -> ofLeft(leftValue);
             case RIGHT -> {
@@ -42,21 +41,21 @@ public class Either<Left, Right> {
         };
     }
 
-    public <KRight> Either<Left, KRight> flatMap(Function<Right, Either<Left, KRight>> transformer) {
+    public <K> Either<L, K> flatMap(Function<? super R, Either<L, K>> transformer) {
         return switch (choice) {
             case LEFT -> ofLeft(leftValue);
             case RIGHT -> transformer.apply(rightValue);
         };
     }
 
-    public <V> V match(Function<Left, V> leftMapper, Function<Right, V> rightMapper) {
+    public <V> V match(Function<L, V> leftMapper, Function<? super R, V> rightMapper) {
         return switch (choice) {
             case LEFT -> leftMapper.apply(leftValue);
             case RIGHT -> rightMapper.apply(rightValue);
         };
     }
 
-    public void matchConsume(Consumer<Left> leftConsumer, Consumer<Right> rightConsumer) {
+    public void matchConsume(Consumer<? super L> leftConsumer, Consumer<? super R> rightConsumer) {
         switch (choice) {
             case LEFT -> leftConsumer.accept(leftValue);
             case RIGHT -> rightConsumer.accept(rightValue);
--- etlagent/src/test/java/hle/etlagent/RunTimeErrorHandleTests.java
+++ etlagent/src/test/java/hle/etlagent/RunTimeErrorHandleTests.java
@@ -51,17 +51,22 @@ class RunTimeErrorHandleTests {
     @SuppressWarnings("resource")
     @Container
     static MariaDBContainer<?> mariadb = new MariaDBContainer<>("mariadb:lts")
-            .withInitScript("initialize_mariadb.sql");
+            .withInitScript("initialize_mariadb.sql")
+            .withNetwork(network)
+            .withNetworkAliases("maria");
+
+    static Proxy mariadbProxy;
 
     @DynamicPropertySource  // non-auto-config datasource is not compatible with Spring boot 3.1 @ServiceConnection
     static void configureProperties(DynamicPropertyRegistry registry) throws IOException {
         var toxiproxyClient = new ToxiproxyClient(toxiproxy.getHost(), toxiproxy.getControlPort());
         postgresqlProxy = toxiproxyClient.createProxy("postgresql", "0.0.0.0:8666", "postgres:5432");
+        mariadbProxy = toxiproxyClient.createProxy("mariadb", "0.0.0.0:8667", "maria:3306");
 
         registry.add("spring.datasource.inline.url", () -> "jdbc:postgresql://%s:%d/%s".formatted(toxiproxy.getHost(), toxiproxy.getMappedPort(8666), postgres.getDatabaseName()));
         registry.add("spring.datasource.inline.username", postgres::getUsername);
         registry.add("spring.datasource.inline.password", postgres::getPassword);
-        registry.add("spring.datasource.litho.url", mariadb::getJdbcUrl);
+        registry.add("spring.datasource.litho.url", () -> "jdbc:mariadb://%s:%d/%s".formatted(toxiproxy.getHost(), toxiproxy.getMappedPort(8667), mariadb.getDatabaseName()));
         registry.add("spring.datasource.litho.username", mariadb::getUsername);
         registry.add("spring.datasource.litho.password", mariadb::getPassword);
     }
@@ -75,7 +80,7 @@ class RunTimeErrorHandleTests {
 
     @Test
     @Order(1)
-    void inlineToLithoFetchNetworkTempCutCoverByRetry() throws IOException {
+    void inlineTempDownCoverByRetry() throws IOException {
         postgresqlProxy.disable();
 
         var from = LocalDateTime.of(2023, 9, 12, 0, 0, 0);
@@ -85,6 +90,22 @@ class RunTimeErrorHandleTests {
         timer.schedule(new ReEnableProxy(postgresqlProxy), 30000);
 
         inspectProcessor.fetchInspRawByWindow(new TimeWindow(from, to));
+        var result = adoptRepo.findByWindow(from, to);
+
+        assertThat(result).hasSize(24);
+    }
+
+    @Test
+    @Order(2)
+    void lithoDbTempDownCoverByRetry() throws IOException {
+        mariadbProxy.disable();
+
+        var from = LocalDateTime.of(2023, 9, 12, 0, 0, 0);
+        var to = LocalDateTime.of(2023, 9, 13, 0, 0, 0);
+
+        Timer timer = new Timer();
+        timer.schedule(new ReEnableProxy(mariadbProxy), 30000);
+
         var result = adoptRepo.findByWindow(from, to);
 
         assertThat(result).hasSize(24);
